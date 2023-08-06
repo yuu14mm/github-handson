a831545227ee92f2e9888626bdcf41d27631d5f6
yuu14mm <124451437+yuu14mm@users.noreply.github.com>
2023-08-06 19:25:24 +0900

    Lesson4
    第４回課題

--- Main.java
+++ Main.java
@@ -1,26 +1,7 @@
 package org.example;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Main {
     public static void main(String[] args) {
-        Map<String, Integer> productMap = new HashMap<>();
-        productMap.put("冷蔵庫", 50000);
-        productMap.put("エアコン", 150000);
-        productMap.put("テレビ", 60000);
-
-        for (Map.Entry<String, Integer> entry : productMap.entrySet()) {
-            System.out.println("商品名：" + entry.getKey() + "　価格：" + entry.getValue());
-        }
-
-        Map<String, Integer> productMap2 = null;
-        try {
-            System.out.println(productMap2.get("パソコン"));
-
-        } catch (NullPointerException e) {
-            System.out.println("取り扱いがありません。");
-
-        }
+        System.out.println("Hello world!");
     }
 }
